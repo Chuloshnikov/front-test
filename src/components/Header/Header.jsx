1ef089f4d8230ed1eb4d868e9564b5cf8f27b9c6
MaxChuloshnikov <Maks447@ukr.net>
2023-03-15 14:49:09 +0200

    Header and hero Done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { HeaderContainer, HeaderWrapper, LogoContainer, ButtonsContainer} from './Header.styled';
+import Logo from '../../assets/icons/Logo.svg';
+import { PrimaryButton } from '../Button/PrimaryButton';
+
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <HeaderContainer>
+        <LogoContainer>
+            <a href="/">
+              <img src={Logo} alt="logo"/>
+            </a>
+        </LogoContainer>
+        <ButtonsContainer>
+          <PrimaryButton text="Users"/>
+          <PrimaryButton text="Sign Up"/>
+        </ButtonsContainer>
+      </HeaderContainer>
+    </HeaderWrapper>
+  )
+}
+
+export default Header
